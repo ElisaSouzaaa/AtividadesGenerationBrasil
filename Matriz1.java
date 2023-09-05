3bbf504e9f0e861a6c1868738414f933703ee3d6
Elisa Souza <102561113+ElisaSouzaaa@users.noreply.github.com>
2023-09-05 01:34:34 -0300

    Add files via upload

---
+++ Matriz1.java
@@ -0,0 +1,37 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class Matriz1 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		int valorMatriz[][] = new int [3][3];
+		int soma1 = 0, soma2 = 0;
+		
+		//Pede os valores ao usuário valores principal
+		for(int indiceLinha = 0; indiceLinha < 3; indiceLinha++) {
+			for(int indiceColuna = 0; indiceColuna < 3; indiceColuna++) {
+				System.out.println("Digite um valor para a posição [" + indiceLinha + "][" + indiceColuna + "]: ");
+				valorMatriz[indiceLinha][indiceColuna] = leia.nextInt();
+				
+				//Aqui imprime e soma os valores na diagonal principal
+				if(indiceLinha == indiceColuna) {
+					soma1 += valorMatriz[indiceLinha][indiceColuna];
+				}
+				
+				if (indiceLinha + indiceColuna == 2) {
+                    soma2 += valorMatriz[indiceLinha][indiceColuna];
+                }
+			}
+		}
+		
+		System.out.println("\nElementos da Diagonal Principal: " + valorMatriz[0][0] + ", " + valorMatriz[1][1] + ", " + valorMatriz[2][2]);
+		System.out.println("Elementos da Diagonal Secundária: " + valorMatriz[0][2] + ", " + valorMatriz[1][1] + ", " + valorMatriz[2][0]);
+		System.out.println("A soma dos elementos da Diagonal Principal é: " + soma1);
+		System.out.println("A soma dos elementos da Diagonal Principal é: " + soma2);
+		
+		leia.close();
+	}
+}
